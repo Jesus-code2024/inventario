ba6bb7dc9108b3cdd134f6830059d563614c2924
Jesus-code2024 <alcides.ruiz@tecsup.edu.pe>
2025-04-09 16:17:32 -0500

    subir migraciones

--- src/inventario/models.py
+++ src/inventario/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+# Modelo Categoría
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+
+# Modelo Proveedor
+class Proveedor(models.Model):
+    nombre = models.CharField(max_length=200)
+    contacto = models.CharField(max_length=100)
+    direccion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+
+# Modelo Producto
+class Producto(models.Model):
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    cantidad = models.PositiveIntegerField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
